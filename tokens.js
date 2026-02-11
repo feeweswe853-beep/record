644ce24af73a926b031c48af7dacea3ddb3876c2
feeweswe853-beep <feeweswe853@gmail.com>
2026-02-11 22:26:03 +0000

    Fix and Update

--- tokens.js
+++ tokens.js
@@ -47,4 +47,20 @@ export default [
         },
         selfMute: true,
     }, 
+
+               {
+        channelId: "1471270870000603280",
+        serverId: "534163543516250114",
+        token: process.env.token4,
+        selfDeaf: false,
+        autoReconnect: {
+            enabled: true,
+            delay: 5, // ثواني
+            maxRetries: 5,
+        },
+        presence: {
+            status: "dnd",
+        },
+        selfMute: true,
+    }, 
 ];
